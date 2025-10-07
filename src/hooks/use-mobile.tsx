4462648fe07fc66ed1cc394cc66b11bb5b9852b5
Jonatan Banda <jonatan.banda.mtz@gmail.com>
2025-10-07 18:42:49 +0000

    Fix sidebar behavior on mobile

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,20 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const handler = () => setIsMobile(mediaQuery.matches)
+    
+    // Initial check
+    handler()
+
+    // Listen for changes
+    mediaQuery.addEventListener("change", handler)
+
+    return () => mediaQuery.removeEventListener("change", handler)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
