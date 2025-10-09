99f71bcb51fb77be9d41d62426d4dc0ce58a4688
Jonatan Banda <jonatan.banda.mtz@gmail.com>
2025-10-09 02:20:02 +0000

    Añade el monitor de progreso y corrige el manejo de datos en el backend

--- .firebase/studio-2799607830-e7b65/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssrstudio2799607830e7b6 = onRequest({}, (req, res) => server.then(it => it.handle(req, res)));
-  
